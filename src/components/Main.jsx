9116bb6262be506975209d335e9b13cf8621c1fe
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-10-23 07:04:13 +0900

    Chore: console 제거

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,4 @@
 export default function Main({ children }) {
-  console.log(children);
   return (
     <main className='px-8 py-20 transition-all duration-300 md:ml-52 max-w-7xl'>
       {children}
