e25a03f0d4b26346333f089ddb75c2be4e88c839
GCgang <powm97@gmail.com>
2024-10-22 18:47:24 +0900

    Feat: ViewToggle

---
+++ src/components/ViewToggle.jsx
@@ -0,0 +1,29 @@
+import { FaTh, FaList } from 'react-icons/fa';
+
+export default function ViewToggle({ isGridView, setIsGridView }) {
+  const handleClick = (view) => {
+    setIsGridView(view);
+  };
+  return (
+    <div className='space-x-2'>
+      <button
+        onClick={() => handleClick(true)}
+        className={`bg-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2  focus:ring-blue-500 ${
+          isGridView ? 'bg-blue-500 text-white' : 'bg-gray-200'
+        }`}
+        aria-label='Grid View'
+      >
+        <FaTh />
+      </button>
+      <button
+        onClick={() => handleClick(false)}
+        className={`bg-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2  focus:ring-blue-500 ${
+          !isGridView ? 'bg-blue-500 text-white' : 'bg-gray-200'
+        }`}
+        aria-label='List View'
+      >
+        <FaList />
+      </button>
+    </div>
+  );
+}
