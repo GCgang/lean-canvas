922a9c42f740e42afc7b7c3ab39c128a0ef2b168
GCgang <powm97@gmail.com>
2024-10-25 19:03:34 +0900

    Feat: loading, error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,28 @@ import SearchBar from '../components/SearchBar';
 import ViewToggle from '../components/ViewToggle';
 import { useEffect } from 'react';
 import { getCanvases } from '../api/cavas';
+import Loading from '../components/Loading';
+import Error from '../components/Error';
 
 export default function Home() {
   const [searchInput, setSearchInput] = useState('');
   const [isGridView, setIsGridView] = useState(true);
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(null);
 
   async function fetchData(params) {
-    const response = await getCanvases(params);
-    setData(response.data);
+    try {
+      setIsLoading(true);
+      setIsError(null);
+      await new Promise((resolver) => setTimeout(resolver, 2000));
+      const response = await getCanvases(params);
+      setData(response.data);
+    } catch (error) {
+      setIsError(error);
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   useEffect(() => {
@@ -23,20 +36,28 @@ export default function Home() {
     setData(data.filter((item) => item.id !== id));
   };
 
+  const handleRetry = () => {
+    fetchData({ title_like: searchInput });
+  };
+
   return (
     <>
       <div className='flex items-center justify-between mb-8'>
         <SearchBar searchInput={searchInput} setSearchInput={setSearchInput} />
         <ViewToggle isGridView={isGridView} setIsGridView={setIsGridView} />
       </div>
-      <section>
-        <CanvasList
-          filteredData={data}
-          searchInput={searchInput}
-          isGridView={isGridView}
-          onDeleteItem={handleDeleteItem}
-        />
-      </section>
+      {isLoading && <Loading />}
+      {isError && <Error message={isError.message} onRetry={handleRetry} />}
+      {!isLoading && !isError && (
+        <section>
+          <CanvasList
+            filteredData={data}
+            searchInput={searchInput}
+            isGridView={isGridView}
+            onDeleteItem={handleDeleteItem}
+          />
+        </section>
+      )}
     </>
   );
 }
