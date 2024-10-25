3a9040e4518de0da33e0e139f5c79d588b7c6788
GCgang <powm97@gmail.com>
2024-10-25 20:06:58 +0900

    Feat: create canvas

--- src/api/cavas.js
+++ src/api/cavas.js
@@ -1,5 +1,16 @@
 import { canvasHttpClient } from './http';
+import { v4 as uuidv4 } from 'uuid';
+import dayjs from 'dayjs';
 
 export function getCanvases(params) {
   return canvasHttpClient.get('/', { params });
+}
+
+export function createCanvas() {
+  const newCanvas = {
+    title: uuidv4().substring(0, 4) + '_새로운 린 캔버스',
+    lastModified: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+    category: '신규',
+  };
+  return canvasHttpClient.post('/', newCanvas);
 }
