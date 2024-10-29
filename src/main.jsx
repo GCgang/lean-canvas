b4df6708f77b99f892d9484a1695ed247845a704
GCgang <powm97@gmail.com>
2024-10-29 14:52:22 +0900

    Feat: tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,17 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import 'normalize.css';
 import './index.css';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
